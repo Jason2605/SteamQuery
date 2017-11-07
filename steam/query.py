f41e5450406b59dcdecba6e79c986e4ae446cd3b
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-11-07 20:09:29 +0000

    some pep8s

--- steam/query.py
+++ steam/query.py
@@ -6,10 +6,14 @@ class SteamQuery:
         self.ip = ip
         self.port = port
         self.timeout = timeout
+        
         self.server_info = None
 
     def query_game_server(self):
-        """Main function that creates the socket connection to the steam API"""
+        """
+        Main function that creates the socket connection to the steam API
+        """
+        
         try:
             udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             udpsock.settimeout(self.timeout)
@@ -28,43 +32,52 @@ class SteamQuery:
             udpsock.close()
 
     def _sort_data(self, data):
-        """Sorts the returned data from the API into a python dictionary"""
-        if data:
-            server_info = {}
-            data = data[6:].split(b'\x00', 4)
-            server_info['online'] = True
-            server_info['ip'] = self.ip
-            server_info['port'] = self.port
-            server_info['name'] = data[0].decode()
-            server_info['map'] = data[1].decode()
-            server_info['game'] = data[2].decode()
-            server_info['description'] = data[3].decode()
-            in_data = data[4]
-            server_info['players'] = in_data[2]
-            server_info['max_players'] = in_data[3]
-            server_info['bots'] = in_data[4]
-            server_info['password_required'] = bool(in_data[7])
-            server_info['vac_secure'] = bool(in_data[8])
-            server_type = chr(in_data[5])
-            os = chr(in_data[6])
-            if server_type == 'd':
-                server_info['server_type'] = 'Dedicated'
-            elif server_type == 'l':
-                server_info['server_type'] = 'Non-Dedicated'
-            else:
-                server_info['server_type'] = 'SourceTV'
-            if os == 'w':
-                server_info['os'] = 'Windows'
-            elif os == 'l':
-                server_info['os'] = 'Linux'
-            else:
-                server_info['os'] = 'Mac'
-            self.server_info = server_info
-            return server_info
-        return None
+        """
+        Sorts the returned data from the API into a python dictionary
+        """
+        
+        if not data:
+            return
+        
+        server_info = {}
+        data = data[6:].split(b'\x00', 4)
+        server_info['online'] = True
+        server_info['ip'] = self.ip
+        server_info['port'] = self.port
+        server_info['name'] = data[0].decode()
+        server_info['map'] = data[1].decode()
+        server_info['game'] = data[2].decode()
+        server_info['description'] = data[3].decode()
+        
+        in_data = data[4]
+        
+        server_info['players'] = in_data[2]
+        server_info['max_players'] = in_data[3]
+        server_info['bots'] = in_data[4]
+        server_info['password_required'] = bool(in_data[7])
+        server_info['vac_secure'] = bool(in_data[8])
+        
+        server_type = chr(in_data[5])
+        os = chr(in_data[6])
+        
+        if server_type == 'd':
+            server_info['server_type'] = 'Dedicated'
+        elif server_type == 'l':
+            server_info['server_type'] = 'Non-Dedicated'
+        else:
+            server_info['server_type'] = 'SourceTV'
+        if os == 'w':
+            server_info['os'] = 'Windows'
+        elif os == 'l':
+            server_info['os'] = 'Linux'
+        else:
+            server_info['os'] = 'Mac'
+        self.server_info = server_info
+        return server_info
 
     def return_last_data(self):
-        """Returns the saved dictionary, or queries if the dictionary hasnt yet been formed"""
-        if self.server_info is None:
-            return self.query_game_server()
-        return self.server_info
+        """
+        Returns the saved dictionary, or queries if the dictionary hasnt yet been formed
+        """
+
+        return self.query_game_server() if self.server_info is None else self.server_info
