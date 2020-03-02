60d2f833c822d7d486b609241bb47b07c4fe3bfd
Tim Schmidt <tim.schmidt@ewe.net>
2020-03-02 06:05:49 +0100

    added A2S_RULES support; fixed endianness

--- steam/query.py
+++ steam/query.py
@@ -1,15 +1,17 @@
 import socket
 import struct
 from steam.player import Player
-from typing import List
+from typing import List, Tuple
 from deprecation import deprecated
 
 HEADER_NO_SPLIT                 = b'\xFF\xFF\xFF\xFF'
+CHALLENGE_HEADER                = b'A'
 A2S_INFO_HEADER                 = b'T'
 A2S_INFO_PAYLOAD                = b'Source Engine Query\x00'
 A2S_PLAYER_REQUEST              = b'U'
-A2S_PLAYER_RESPONSE_CHALLENGE   = b'A'
-A2S_PLAYER_RESPONSE_DATA        = b'D'
+A2S_PLAYER_RESPONSE             = b'D'
+A2S_RULES_REQUEST               = b'V'
+A2S_RULES_RESPONSE              = b'E'
 
 class SteamQuery:
     def __init__(self, ip='127.0.0.1', port=2303, timeout=1):
@@ -64,14 +66,14 @@ class SteamQuery:
             # Get challenge number
             payload = A2S_PLAYER_REQUEST + challenge_number
             response = self._make_request(udpsock, payload)
-            assert response.startswith(A2S_PLAYER_RESPONSE_CHALLENGE)
+            assert response.startswith(CHALLENGE_HEADER)
             challenge_number = response[1:5]
 
             # Send new request with challenge number
             payload = A2S_PLAYER_REQUEST + challenge_number
             response = self._make_request(udpsock, payload)
 
-            assert response.startswith(A2S_PLAYER_RESPONSE_DATA)
+            assert response.startswith(A2S_PLAYER_RESPONSE)
             player_amount = response[1]
             player_data = response[2:]
 
@@ -80,7 +82,7 @@ class SteamQuery:
             for i in range(player_amount):
                 index = player_data[0]
                 name, _, player_data = player_data[1:].partition(b"\x00")
-                score, duration = struct.unpack(">if", player_data[:8])
+                score, duration = struct.unpack("<if", player_data[:8])
                 player_data = player_data[8:]
                 player = Player(index, name, score, duration)
                 players.append(player)
@@ -88,6 +90,53 @@ class SteamQuery:
             assert len(player_data) == 0
 
             return players
+
+        except socket.timeout:
+            return {'error': 'Request timed out'}
+        except Exception as e:
+            return {'error': str(e)}
+        finally:
+            udpsock.close()
+
+    def query_server_config(self) -> dict:
+        """
+        Queries configuration values that are currently in use on the server
+        using the A2S_RULES command.
+        """
+
+        try:
+            udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udpsock.settimeout(self.timeout)
+            udpsock.connect((self.ip, self.port))
+
+            # Same as with A2S_PLAYER, request a challenge
+            challenge_number = b'\xFF\xFF\xFF\xFF'
+
+            # Get challenge number
+            payload = A2S_RULES_REQUEST + challenge_number
+            response = self._make_request(udpsock, payload)
+            assert response.startswith(CHALLENGE_HEADER)
+            challenge_number = response[1:5]
+
+            # Send new request with challenge number
+            payload = A2S_RULES_REQUEST + challenge_number
+            response = self._make_request(udpsock, payload)
+
+            assert response.startswith(A2S_RULES_RESPONSE)
+            rules_amount = struct.unpack("<H", response[1:3])[0]
+            rules_data = response[3:]
+
+            # Unpack rule data
+            # Rules are stored as null-terminated k,v string pairs.
+            # Just split on null-bytes and then
+            # zip the list with itself to get the pairs.
+            rules_strings = rules_data.split(b"\x00")
+            rules_pairs = zip(rules_strings[::2], rules_strings[1::2])
+            rules = dict(rules_pairs)
+
+            assert len(rules) == rules_amount
+
+            return rules
 
         except socket.timeout:
             return {'error': 'Request timed out'}
