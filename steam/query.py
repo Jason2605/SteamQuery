104fc76b3fc17c964d838ba10f737418c8542ca9
Tim Schmidt <tim.schmidt@ewe.net>
2020-03-03 00:16:25 +0100

    added retry mechanism for challenges;
    - moved UDP connection handling and error catching into unified decorator method
    - added error messages for split packages (unsupported) and servers sending too many retries
    - added support for A2S_PLAYERS responses with more than 255 players

--- steam/query.py
+++ steam/query.py
@@ -1,10 +1,13 @@
+import functools
 import socket
 import struct
+from deprecation import deprecated
 from steam.player import Player
 from typing import List, Tuple
-from deprecation import deprecated
+
 
 HEADER_NO_SPLIT                 = b'\xFF\xFF\xFF\xFF'
+HEADER_SPLIT                    = b'\xFF\xFF\xFF\xFE'
 CHALLENGE_HEADER                = b'A'
 A2S_INFO_HEADER                 = b'T'
 A2S_INFO_PAYLOAD                = b'Source Engine Query\x00'
@@ -12,6 +15,29 @@ A2S_PLAYER_REQUEST              = b'U'
 A2S_PLAYER_RESPONSE             = b'D'
 A2S_RULES_REQUEST               = b'V'
 A2S_RULES_RESPONSE              = b'E'
+
+
+class BrokenChallengeError(Exception):
+    pass
+
+
+def _query_function(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udpsock.settimeout(self.timeout)
+            udpsock.connect((self.ip, self.port))
+
+            return func(self, *args, **kwargs, udpsock=udpsock)
+        except socket.timeout:
+            return {'error': 'Request timed out'}
+        except Exception as e:
+            return {'error': str(e)}
+        finally:
+            udpsock.close()
+    return wrapper
+
 
 class SteamQuery:
     def __init__(self, ip='127.0.0.1', port=2303, timeout=1):
@@ -29,130 +55,100 @@ class SteamQuery:
         """
         Queries game server information using the A2S_INFO command.
         """
+        return self._query_server_info()
 
-        try:
-            udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            udpsock.settimeout(self.timeout)
-            udpsock.connect((self.ip, self.port))
-            payload = A2S_INFO_HEADER + A2S_INFO_PAYLOAD
-            data = self._make_request(udpsock, payload)
-            server_info = self._unpack_server_data(data)
-            if server_info is not None:
-                return server_info
-            return {'online': False, 'error': 'Unknown error'}
-        except socket.timeout:
-            return {'online': False, 'error': 'Request timed out'}
-        except Exception as e:
-            return {'online': False, 'error': str(e)}
-        finally:
-            udpsock.close()
+    @_query_function
+    def _query_server_info(self, *, udpsock) -> dict:
+        payload = A2S_INFO_HEADER + A2S_INFO_PAYLOAD
+        data = self._make_request(udpsock, payload)
+        server_info = self._unpack_server_data(data)
+        if server_info is not None:
+            return server_info
+        return {'online': False, 'error': 'Unknown error'}
 
     def query_player_info(self) -> List[Player]:
         """
         Queries information about players that are currently on the server
         using the A2S_PLAYER command.
         """
+        return self._query_player_info()
 
-        try:
-            udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            udpsock.settimeout(self.timeout)
-            udpsock.connect((self.ip, self.port))
+    @_query_function
+    def _query_player_info(self, *, udpsock) -> List[Player]:
+        response = self._make_challenged_request(udpsock, A2S_PLAYER_REQUEST)
 
-            # The A2S_PLAYER command requires a challenge-response.
-            # On initial connection, set challenge number to -1 (FFFFFFFF)
-            # to request a challenge number.
-            challenge_number = b'\xFF\xFF\xFF\xFF'
+        assert response.startswith(A2S_PLAYER_RESPONSE)
+        # NOTE: player_amount is a signed 8-bit integer.
+        # Servers with more than 255 players might return additional player
+        # information.
+        # We thus ignore this value and just parse the player data.
+        player_amount = response[1]
+        player_data = response[2:]
 
-            # Get challenge number
-            payload = A2S_PLAYER_REQUEST + challenge_number
-            response = self._make_request(udpsock, payload)
-            assert response.startswith(CHALLENGE_HEADER)
-            challenge_number = response[1:5]
+        # Unpack player data
+        players = []
+        while len(player_data) > 0:
+            index = player_data[0]
+            name, _, player_data = player_data[1:].partition(b"\x00")
+            name = name.decode()
+            score, duration = struct.unpack("<if", player_data[:8])
+            player_data = player_data[8:]
+            player = Player(index, name, score, duration)
+            players.append(player)
 
-            # Send new request with challenge number
-            payload = A2S_PLAYER_REQUEST + challenge_number
-            response = self._make_request(udpsock, payload)
+        assert len(player_data) == 0
 
-            assert response.startswith(A2S_PLAYER_RESPONSE)
-            player_amount = response[1]
-            player_data = response[2:]
-
-            # Unpack player data
-            players = []
-            for i in range(player_amount):
-                index = player_data[0]
-                name, _, player_data = player_data[1:].partition(b"\x00")
-                name = name.decode()
-                score, duration = struct.unpack("<if", player_data[:8])
-                player_data = player_data[8:]
-                player = Player(index, name, score, duration)
-                players.append(player)
-
-            assert len(player_data) == 0
-
-            return players
-
-        except socket.timeout:
-            return {'error': 'Request timed out'}
-        except Exception as e:
-            return {'error': str(e)}
-        finally:
-            udpsock.close()
+        return players
 
     def query_server_config(self) -> dict:
         """
         Queries configuration values that are currently in use on the server
         using the A2S_RULES command.
         """
+        return self._query_server_config()
 
-        try:
-            udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            udpsock.settimeout(self.timeout)
-            udpsock.connect((self.ip, self.port))
+    @_query_function
+    def _query_server_config(self, *, udpsock) -> dict:
+        response = self._make_challenged_request(udpsock, A2S_RULES_REQUEST)
 
-            # Same as with A2S_PLAYER, request a challenge
-            challenge_number = b'\xFF\xFF\xFF\xFF'
+        assert response.startswith(A2S_RULES_RESPONSE)
+        rules_amount = struct.unpack("<H", response[1:3])[0]
+        rules_data = response[3:]
 
-            # Get challenge number
-            payload = A2S_RULES_REQUEST + challenge_number
-            response = self._make_request(udpsock, payload)
-            assert response.startswith(CHALLENGE_HEADER)
-            challenge_number = response[1:5]
+        # Unpack rule data
+        # Rules are stored as null-terminated k,v string pairs.
+        # Just split on null-bytes and then
+        # zip the list with itself to get the pairs.
+        rules_strings = rules_data.split(b"\x00")
+        rules_strings = [b.decode() for b in rules_strings]
+        rules_pairs = zip(rules_strings[::2], rules_strings[1::2])
+        rules = dict(rules_pairs)
 
-            # Send new request with challenge number
-            payload = A2S_RULES_REQUEST + challenge_number
-            response = self._make_request(udpsock, payload)
+        assert len(rules) == rules_amount
 
-            assert response.startswith(A2S_RULES_RESPONSE)
-            rules_amount = struct.unpack("<H", response[1:3])[0]
-            rules_data = response[3:]
+        return rules
 
-            # Unpack rule data
-            # Rules are stored as null-terminated k,v string pairs.
-            # Just split on null-bytes and then
-            # zip the list with itself to get the pairs.
-            rules_strings = rules_data.split(b"\x00")
-            rules_strings = [b.decode() for b in rules_strings]
-            rules_pairs = zip(rules_strings[::2], rules_strings[1::2])
-            rules = dict(rules_pairs)
-
-            assert len(rules) == rules_amount
-
-            return rules
-
-        except socket.timeout:
-            return {'error': 'Request timed out'}
-        except Exception as e:
-            return {'error': str(e)}
-        finally:
-            udpsock.close()
-
-    def _make_request(self, socket, payload):
+    def _make_request(self, udpsock, payload):
         data = HEADER_NO_SPLIT + payload
-        socket.send(data)
-        data = socket.recv(4096)
+        udpsock.send(data)
+        data = udpsock.recv(81920)
+        if data.startswith(HEADER_SPLIT):
+            raise NotImplementedError("Split packages are not supported.")
         assert data.startswith(HEADER_NO_SPLIT)
         return data[4:]
+
+    def _make_challenged_request(self, udpsock, payload_prefix):
+        MAX_RETRIES = 10
+        challenge_number = b"\xFF\xFF\xFF\xFF"
+        for i in range(MAX_RETRIES):
+            payload = payload_prefix + challenge_number
+            response = self._make_request(udpsock, payload)
+            if not response.startswith(CHALLENGE_HEADER):
+                return response
+            challenge_number = response[1:5]
+        raise BrokenChallengeError("Server keeps sending new challenges "
+                                   "instead of accepting.")
+
 
     def _unpack_server_data(self, data):
         """
